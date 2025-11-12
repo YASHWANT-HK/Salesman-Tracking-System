90392d1ef20a6ec859e8c957f2d25d248ecf7612
Yashwanth K <yashkgr2003@gmail.com>
2025-11-12 23:01:37 +0530

    working on signup restful api, error handling middlewear

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 dotenv.config();
 const connectMongodb = require("./init/mongodb");
+const { authRoute } = require("./routes");
+const { errorHandler } = require("./middlewares");
 
 //initialising app
 const app = express();
@@ -13,6 +16,13 @@ connectMongodb();
 //third-party middleware
 app.use(express.json({ limit: "500mb" }));
 app.use(bodyParser.urlencoded({ limit: "500mb", extended: true }));
+app.use(morgan("dev"));
+
+//route section
+app.use("/api/v1/auth", authRoute);
+
+//error handling middleware
+app.use(errorHandler);
 
 module.exports = app;
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const { authController } = require("../controllers");
 
-router.post("/signup", )
+router.post("/signup", authController.signup );
 
 module.exports = router;
